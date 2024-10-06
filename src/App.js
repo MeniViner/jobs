4733563bd6a7a98854dfa72152b3b94bb903e446
MeniViner <meniviner@gmail.com>
2024-10-07 00:12:16 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { AuthProvider } from './contexts/AuthContext';
-import theme from './theme';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 import PostJob from './pages/PostJob';
 import AdminMessagesDashboard from './pages/AdminMessagesDashboard';
 import Header from './components/Header';
@@ -12,41 +14,45 @@ import JobListPage from './pages/JobListPage';
 import ProfileComponents from './components/ProfileComponents';
 import EmployerProfile from './pages/EmployerProfile';
 import ManageUsers from './pages/Management/ManageUsers';
-import LoginPage from './pages/profile/loginPage'
-import AccountPage from './pages/profile/AccountPage'
+import LoginPage from './pages/profile/loginPage';
+import AccountPage from './pages/profile/AccountPage';
 
+// Create a theme with RTL support
+const theme = createTheme({
+  direction: 'rtl',  // Right-to-left support
+});
+
+// Create RTL cache
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [rtlPlugin],
+});
 
 function App() {
   return (
     <AuthProvider>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/jobs" element={<JobListPage />} />
-            <Route path="/employer-register" element={<ProfileComponents.RegisterPage />} />
-            <Route path="/post-job" element={<PostJob />} />
-            <Route path="/admin/messages" element={<AdminMessagesDashboard />} />
-            <Route path="/ManageUsers" element={<ManageUsers />} />
-            <Route path="/account" element={<AccountPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/employer-profile" element={<EmployerProfile />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
+      <CacheProvider value={cacheRtl}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Router>
+            <Header />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/jobs" element={<JobListPage />} />
+              <Route path="/employer-register" element={<ProfileComponents.RegisterPage />} />
+              <Route path="/post-job" element={<PostJob />} />
+              <Route path="/admin/messages" element={<AdminMessagesDashboard />} />
+              <Route path="/ManageUsers" element={<ManageUsers />} />
+              <Route path="/account" element={<AccountPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/employer-profile" element={<EmployerProfile />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </Router>
+        </ThemeProvider>
+      </CacheProvider>
     </AuthProvider>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-

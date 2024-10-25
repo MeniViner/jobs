a9d852261811228dd0d777419b5f5559e45947eb
MeniViner <meniviner@gmail.com>
2024-10-25 19:14:46 +0300

    .

--- src/pages/CompanyInfoPage.js
+++ src/pages/CompanyInfoPage.js
@@ -6,8 +6,8 @@ import {
 import { WhatsApp, Telegram, Share, LinkedIn, Twitter, Work, People, Diversity3, TrendingUp } from '@mui/icons-material';
 import { styled } from '@mui/system';
 import { motion } from 'framer-motion';
-import f1 from '../images/1.jpg';
-import f2 from '../images/2.jpg';
+import f1 from '../images/mv.jpg';
+import f2 from '../images/yc.jpg';
 const AnimatedBox = motion(Box);
 
 const ContactButton = styled(Button)({
